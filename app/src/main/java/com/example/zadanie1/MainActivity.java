191b45166d7a3e9f1f737602a31775e74ee151b9
mivchal <mivchal@hotmail.com>
2020-10-07 22:24:25 +0200

    quiz works

--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -3,12 +3,69 @@ package com.example.zadanie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import lombok.SneakyThrows;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+    private TextView textView;
+    private Question[] questions = {
+            new Question(R.string.question_1, true),
+            new Question(R.string.question_2, true),
+            new Question(R.string.question_3, true),
+            new Question(R.string.question_4, true)
+    };
+    private int currentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.button_true);
+        falseButton = findViewById(R.id.button_false);
+        textView = findViewById(R.id.textView);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @SneakyThrows
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(true);
+                Thread.sleep(1000);
+                setNextQuestion();
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @SneakyThrows
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(false);
+                Thread.sleep(1000);
+                setNextQuestion();
+            }
+        });
+        textView.setText(questions[0].getQuestionId());
+    }
+
+    private void setNextQuestion() {
+        int newIndex =  ++currentIndex % questions.length;
+        textView.setText(questions[newIndex].getQuestionId());
+    }
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId;
+        if (userAnswer == correctAnswer) {
+            resultMessageId = R.string.correct_answer;
+        } else {
+            resultMessageId = R.string.bad_answer;
+        }
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/zadanie1/Question.java
@@ -0,0 +1,14 @@
+package com.example.zadanie1;
+
+import lombok.Data;
+
+@Data
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer) {
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+}
