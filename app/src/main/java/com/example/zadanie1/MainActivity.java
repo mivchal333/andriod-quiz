f7206f4e4edff23c3bf8d61a0d8dd9893a011795
mivchal <mivchal@hotmail.com>
2020-10-22 22:46:32 +0200

    next handled

--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.zadanie1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,15 +8,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import lombok.SneakyThrows;
 
 public class MainActivity extends AppCompatActivity {
-private static final String QUIZ_TAG = "MainActivity";
-private static final String KEY_CURRENT_INDEX = "currentIndex";
-public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    private static final String QUIZ_TAG = "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private boolean answerWasShown;
+
+
     private Button trueButton;
+
     private Button falseButton;
     private Button helpButton;
+    private Button nextButton;
     private TextView textView;
     private Question[] questions = {
             new Question(R.string.question_1, true),
@@ -34,6 +41,16 @@ public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
         super.onStart();
         Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStart");
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) return;
+        if (requestCode == REQUEST_CODE_PROMPT) {
+            if (data == null) return;
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
     }
 
     @Override
@@ -77,13 +94,14 @@ public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
         Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
         }
 
         trueButton = findViewById(R.id.button_true);
         falseButton = findViewById(R.id.button_false);
         helpButton = findViewById(R.id.button_help);
+        nextButton = findViewById(R.id.button_next);
         textView = findViewById(R.id.textView);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
@@ -104,7 +122,10 @@ public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
                 setNextQuestion();
             }
         });
-
+        nextButton.setOnClickListener((v) -> {
+            answerWasShown = false;
+            setNextQuestion();
+        });
         helpButton.setOnClickListener(new View.OnClickListener() {
             @SneakyThrows
             @Override
@@ -112,24 +133,29 @@ public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
                 Intent intent = new Intent(MainActivity.this, PromptActivity.class);
                 boolean correctAnswer = questions[currentIndex].isTrueAnswer();
                 intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
             }
         });
         textView.setText(questions[currentIndex].getQuestionId());
     }
 
     private void setNextQuestion() {
-        int newIndex =  ++currentIndex % questions.length;
-        textView.setText(questions[newIndex].getQuestionId());
+        currentIndex = ++currentIndex % questions.length;
+        textView.setText(questions[currentIndex].getQuestionId());
     }
 
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId;
-        if (userAnswer == correctAnswer) {
-            resultMessageId = R.string.correct_answer;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
         } else {
-            resultMessageId = R.string.bad_answer;
+
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            } else {
+                resultMessageId = R.string.bad_answer;
+            }
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/com/example/zadanie1/PromptActivity.java
+++ app/src/main/java/com/example/zadanie1/PromptActivity.java
@@ -1,16 +1,18 @@
 package com.example.zadanie1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PromptActivity extends AppCompatActivity {
     private boolean correctAnswer;
     private Button showButton;
     private TextView answerTextView;
 
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "answerShown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,13 @@ public class PromptActivity extends AppCompatActivity {
         showButton.setOnClickListener((v) -> {
             int answer = correctAnswer ? R.string.button_true : R.string.button_false;
             answerTextView.setText(answer);
+            setAnswerShownResult(true);
         });
+    }
 
+    private void setAnswerShownResult(boolean answerWasShown) {
+        Intent intent = new Intent();
+        intent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, intent);
     }
 }
